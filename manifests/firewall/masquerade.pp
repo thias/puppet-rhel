78e9855433fd1fe6f781b80a402ba70c7194b4e6
Adria <adria.bisbe@gmail.com>
2017-11-03 14:21:04 +0100

    Allow masquerading multiple interfaces

--- manifests/firewall/masquerade.pp
+++ manifests/firewall/masquerade.pp
@@ -13,63 +13,72 @@ class rhel::firewall::masquerade (
     proto  => 'all',
   }
 
-  if $return_local {
-    firewall { "${return_local_prefix} nomasq to 10.0.0.0/8 out through ${outiface}":
+  # Check if outiface is an Array, otherwise convert it to one
+  if !$outiface.is_a(Array) {
+    $outiface_final = [ $outiface ]
+  } else {
+    $outiface_final = $outiface
+  }
+
+  $outiface_final.each |$iface| {
+    if $return_local {
+      firewall { "${return_local_prefix} nomasq to 10.0.0.0/8 out through ${iface}":
+        chain       => 'POSTROUTING',
+        jump        => 'RETURN',
+        outiface    => $iface,
+        destination => '10.0.0.0/8',
+        table       => 'nat',
+      }
+      firewall { "${return_local_prefix} nomasq to 172.16.0.0/12 out through ${iface}":
+        chain       => 'POSTROUTING',
+        jump        => 'RETURN',
+        outiface    => $iface,
+        destination => '172.16.0.0/12',
+        table       => 'nat',
+      }
+      firewall { "${return_local_prefix} nomasq to 192.168.0.0/16 out through ${iface}":
+        chain       => 'POSTROUTING',
+        jump        => 'RETURN',
+        outiface    => $iface,
+        destination => '192.168.0.0/16',
+        table       => 'nat',
+      }
+    }
+
+    firewall { "${prefix} forward out through ${iface}":
+      action   => 'accept',
+      chain    => 'FORWARD',
+      outiface => $iface,
+      table    => 'filter',
+    }
+    firewall { "${prefix} masq 10.0.0.0/8 out through ${iface}":
       chain    => 'POSTROUTING',
-      jump     => 'RETURN',
-      outiface => $outiface,
-      destination => '10.0.0.0/8',
+      jump     => 'MASQUERADE',
+      outiface => $iface,
+      source   => '10.0.0.0/8',
       table    => 'nat',
     }
-    firewall { "${return_local_prefix} nomasq to 172.16.0.0/12 out through ${outiface}":
+    firewall { "${prefix} masq 172.16.0.0/12 out through ${iface}":
       chain    => 'POSTROUTING',
-      jump     => 'RETURN',
-      outiface => $outiface,
-      destination => '172.16.0.0/12',
+      jump     => 'MASQUERADE',
+      outiface => $iface,
+      source   => '172.16.0.0/12',
       table    => 'nat',
     }
-    firewall { "${return_local_prefix} nomasq to 192.168.0.0/16 out through ${outiface}":
+    firewall { "${prefix} masq 192.168.0.0/16 out through ${iface}":
       chain    => 'POSTROUTING',
-      jump     => 'RETURN',
-      outiface => $outiface,
-      destination => '192.168.0.0/16',
+      jump     => 'MASQUERADE',
+      outiface => $iface,
+      source   => '192.168.0.0/16',
       table    => 'nat',
     }
   }
 
-  firewall { "${prefix} forward out through ${outiface}":
-    action   => 'accept',
-    chain    => 'FORWARD',
-    outiface => $outiface,
-    table    => 'filter',
-  }
+  # Common rules
   firewall { "${prefix} state related established accept":
     action => 'accept',
     chain  => 'FORWARD',
     state  => [ 'RELATED', 'ESTABLISHED' ],
   }
-
-  firewall { "${prefix} masq 10.0.0.0/8 out through ${outiface}":
-    chain    => 'POSTROUTING',
-    jump     => 'MASQUERADE',
-    outiface => $outiface,
-    source   => '10.0.0.0/8',
-    table    => 'nat',
-  }
-  firewall { "${prefix} masq 172.16.0.0/12 out through ${outiface}":
-    chain    => 'POSTROUTING',
-    jump     => 'MASQUERADE',
-    outiface => $outiface,
-    source   => '172.16.0.0/12',
-    table    => 'nat',
-  }
-  firewall { "${prefix} masq 192.168.0.0/16 out through ${outiface}":
-    chain    => 'POSTROUTING',
-    jump     => 'MASQUERADE',
-    outiface => $outiface,
-    source   => '192.168.0.0/16',
-    table    => 'nat',
-  }
-
 }
 
